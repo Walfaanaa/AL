75827c05bb245a9c09617e09fddac059cca6de19
Walfaanaa Magarsaa Ejeta <119677853+Walfaanaa@users.noreply.github.com>
2026-07-13 13:41:35 +0300

    Update app.py

--- app.py
+++ app.py
@@ -5,5 +5,5 @@ EXCEL_URL = "https://raw.githubusercontent.com/Walfaanaa/ATG/main/ATG.xlsx"
 
 df = pd.read_excel(EXCEL_URL, engine="openpyxl")
 
-st.title("Sirna To`annaa Buusii Ji`aa fi Dabalataa Afoosha Tokkummaa Gaalessaa")
+st.title("Sirna To`annaa Buusii Jihaa fi Dabalataa Afoosha Tokkummaa Gaalessaa")
 st.dataframe(df)
